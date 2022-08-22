b4e5e4ded6decb3f966649fae368803d76f7e01f
Fernando Sapene <62686821+fdxcode@users.noreply.github.com>
2022-08-21 20:08:57 -0400

    Update profile.routes.js

--- profile/routes/profile.routes.js
+++ profile/routes/profile.routes.js
@@ -1,22 +1,7 @@
 const controller = require("../controllers/profile.controller");
 module.exports = function (app) {
-  const multer = require("multer");
-
-    //multer middelware
-    let storage = multer.diskStorage({
-        destination: function(req, file, cb){
-            cb(null, 'uploads');
-        },
-        filename: function(req, file, cb){
-            cb(null, file.fieldname+ "_" + Date.now() + "_" + file.originalname);
-        },
-    });
-
-    let upload = multer({
-        storage: storage,
-    }).single("image");
-
-  app.post("/api/profile/:id", upload, controller.createProfile);
+  
+  app.post("/api/profile/:id", controller.createProfile);
   app.get("/api/profile", controller.showProfile);
   app.get("/api/profile/:id", controller.showProfileById);
 };
